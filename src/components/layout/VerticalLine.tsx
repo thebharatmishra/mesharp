b2d2b77fc1edc0e1e999fa64bdc9aa03959372f8
Bharat Mishra <mishrabm2004@gmail.com>
2026-01-20 11:25:06 +0530

    amiup

--- src/components/layout/VerticalLine.tsx
+++
@@ -1,9 +0,0 @@
-import classNames from "classnames";
-
-export function VerticalLine(props: { className?: string }) {
-  return (
-    <div className={classNames("w-full grid justify-center", props.className)}>
-      <div className="w-px h-10 bg-onboarding-divider" />
-    </div>
-  );
-}
